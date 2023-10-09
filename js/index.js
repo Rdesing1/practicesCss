a1e694870b9ce8059e4cd49b6838e1907ad6b93f
Rdesing1 <r.desing4000@gmail.com>
2023-10-09 17:15:15 -0400

    primer proyecto de html, css y javaScript finalizado.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let $img = document.getElementById('img-header');
+
+let $btns = document.querySelectorAll('.btn');
+console.log($btns);
+
+$btns[0].addEventListener('click',(e)=>{
+    $img.setAttribute('src','../../assets/images/rick.png');
+});
+
+$btns[1].addEventListener('click',(e)=>{
+    $img.setAttribute('src','../../assets/images/southpark.jpg');
+});
+
+$btns[2].addEventListener('click',(e)=>{
+    $img.setAttribute('src','../../assets/images/futurama.jpg');
+});
